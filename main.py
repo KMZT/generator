8168f552ae59d374695587c1fb080aba1a33b50d
KMZT <81222923+KMZT@users.noreply.github.com>
2024-09-28 22:21:31 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests, time
+while True:
+  valid_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+  rancookie = ''.join((random.choice(valid_letters) for i in range(1356)))
+  finalcookie = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_" + rancookie
+  print(finalcookie)
+  time.sleep(3)

